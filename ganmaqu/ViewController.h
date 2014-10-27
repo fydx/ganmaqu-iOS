0e7441b89d365cae898d03a710b7089bce376406
潇侠 <xiaoxia.lb@alibaba-inc.com>
2014-10-27 13:56:04 +0800

    Initial Commit

---
+++ ganmaqu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ganmaqu
+//
+//  Created by LiBin on 14/10/27.
+//  Copyright (c) 2014年 LiBin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ganmaqu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ganmaqu
+//
+//  Created by LiBin on 14/10/27.
+//  Copyright (c) 2014年 LiBin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
