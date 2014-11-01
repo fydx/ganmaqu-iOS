f0fb0ea8acc67cbb46dae1bab0104aa801063446
fydx <lbgg918@gmail.com>
2014-11-01 15:38:32 +0800

    增加 首页云彩动画

--- ganmaqu/ViewController.m
+++ ganmaqu/ViewController.m
@@ -14,6 +14,8 @@
 @property (strong, nonatomic) IBOutlet UIButton *typeButton;
 @property (strong, nonatomic) IBOutlet UIButton *confirmButton;
 @property UIAlertView *alert;
+@property (strong, nonatomic) IBOutlet UIImageView *rightCloudImageView;
+@property (strong, nonatomic) IBOutlet UIImageView *leftCloudImageView;
 
 @end
 
@@ -33,27 +35,60 @@
     // Dispose of any resources that can be recreated.
 }
 - (IBAction)popupTypeSelectAlertView:(id)sender {
-     self.alert = [[UIAlertView alloc]initWithTitle:@"请选择出行类型" message:nil delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"亲子出行", @"家人出行", @"情侣出行",nil];
+     self.alert = [[UIAlertView alloc]initWithTitle:@"请选择出行类型" message:nil delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"亲子出行", @"朋友出行", @"情侣出行",nil];
     [self.alert show];
 }
 
 -(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
 {
-    NSLog(@"clickButtonAtIndex:%ld",(long)buttonIndex);
-    NSLog(@"text %@",[self.alert buttonTitleAtIndex:buttonIndex]);
-    [self.typeButton setTitle:[self.alert buttonTitleAtIndex:buttonIndex] forState:UIControlStateNormal] ;
-}
+   
+    //NSLog(@"clickButtonAtIndex:%ld",(long)buttonIndex);
+    //NSLog(@"text %@",[self.alert buttonTitleAtIndex:buttonIndex]);
+    if (buttonIndex > 0) {
+        [self.typeButton setTitle:[self.alert buttonTitleAtIndex:buttonIndex] forState:UIControlStateNormal] ;
+    }
+   }
 - (void)setWidget
 {
     //设置推荐商圈按钮样式
-//    CGRect recommandFrame ;
-//    recommandFrame = self.recommandButton.frame;
-//    recommandFrame.size.width = 1000.0;
-//    self.recommandButton.frame = recommandFrame;
+    //CGRect recommandFrame ;
+    //recommandFrame = self.recommandButton.frame;
+    //recommandFrame.size.width = 1000.0;
+    //self.recommandButton.frame = recommandFrame;
     //[self.recommandButton setBackgroundColor:[UIColor ]];
     _recommandButton.backgroundColor = UIColorFromRGB(0xffbb33);
     _recommandButton.layer.cornerRadius = 3.0;
     _typeButton.backgroundColor = UIColorFromRGB(0x99cc00);
     _typeButton.layer.cornerRadius = 3.0;
+    [self setLeftCloudAnimation];
+    [self setRightCloudAnimation];
+}
+
+- (void)setLeftCloudAnimation
+{
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:4.0];
+    [UIView setAnimationRepeatCount:20.0f];
+    [UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
+    [UIView setAnimationRepeatAutoreverses:YES];
+
+    CGRect rightFrame = self.rightCloudImageView.frame;
+    rightFrame.origin.x -= 50;
+    [self.rightCloudImageView setFrame:rightFrame];
+    
+    [UIView commitAnimations];
+}
+- (void)setRightCloudAnimation
+{
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:4.2];
+    [UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
+    [UIView setAnimationRepeatAutoreverses:YES];
+    [UIView setAnimationRepeatCount:20.0f];
+
+    CGRect leftFrame = self.leftCloudImageView.frame;
+    leftFrame.origin.x += 50;
+    [self.leftCloudImageView setFrame:leftFrame];
+       [UIView commitAnimations];
 }
 @end
